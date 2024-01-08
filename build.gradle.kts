cb4f583b9c59c216ed6cbbafdfb42482a3d9b02d
Tomasz Ziola <tomek.ziola@gmail.com>
2024-01-08 16:12:48 +0100

    added catalog for yml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     jacoco
     checkstyle
-    id("org.springframework.boot") version "3.2.1"
+    id("org.springframework.boot") version "3.2.1-SNAPSHOT"
     id("io.spring.dependency-management") version "1.1.4"
     id("io.freefair.lombok") version "8.4"
 }
