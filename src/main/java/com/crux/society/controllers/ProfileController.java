0d741ee14f57a0e1bf32ca346ff4199a5ec9c7e5
Tomasz Ziola <tomek.ziola@gmail.com>
2024-01-08 15:04:33 +0100

    added catalog for yml file

--- src/main/java/com/crux/society/controllers/ProfileController.java
+++ src/main/java/com/crux/society/controllers/ProfileController.java
@@ -22,7 +22,7 @@ public class ProfileController {
 
   @PostMapping("/register")
   public Mono<ProfileResponseDto> registerProfile(@Valid @RequestBody RegisterProfileDto dto) {
-    return service.registerProfile(dto);
+    return servicee.registerProfile(dto);
   }
 
   @GetMapping("/{id}")
