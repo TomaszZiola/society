5332fb98e57ea010bb7a932d0a0ab8eca7974e9b
Tomasz Ziola <tomek.ziola@gmail.com>
2024-01-08 16:22:35 +0100

    added catalog for yml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ java {
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/snapshot")
+    }
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "society"
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.spring.io/snapshot")
+        }
+        gradlePluginPortal()
+    }
+}
